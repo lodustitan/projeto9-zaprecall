5ab23689c0929ad2c0cebc10b9951da214dad9bd
lodustitan <felipealbertooliveira@gmail.com>
2022-10-03 01:43:57 -0300

    feat perguntas adicionadas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Pergunta from './Components/Pergunta';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function ZapRecall({dados}) {
+
+	const [respondidas, setRespondidas] = React.useState(0);
+	// const [todasResp, setTodasResp] = React.useState(dados.length);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{dados.map((data, index)=>{
+					return <Pergunta index={index+1} pergunta={data.Question} resposta={data.Answer} respondidas={{respondidas, setRespondidas}} />
+				})}
+			</header>
+			<footer>
+				<p>{respondidas}/{dados.length} CONCLUÍDOS</p>
+			</footer>
+		</div>
+	);
 }
 
-export default App;
+export default ZapRecall;
---
+++ src/Components/Pergunta.js
@@ -0,0 +1,158 @@
+import React from "react";
+import styled from "styled-components";
+
+import { FiPlay } from "react-icons/fi";
+import { BsXCircleFill, BsQuestionCircleFill, BsFillCheckCircleFill } from "react-icons/bs";
+import { IoReloadOutline } from "react-icons/io5";
+
+
+
+
+export default function Pergunta(props){
+
+    const [ step, setStep ] = React.useState(0);
+    const [ choice, setChoice ] = React.useState(1);
+
+    return(
+        <Base theme={step}>
+            {step===0 && <FirstStep text={"Pergunta "+props.index} setStep={setStep} />}
+            {step===1 && <SecondStep text={props.pergunta} setStep={setStep} />}
+            {step===2 && <ThirdStep text={props.resposta} setStep={setStep} setChoice={setChoice} ZapStatus={props.respondidas} />}
+            {step===3 && <FourthStep text={"Pergunta "+props.index} status={choice} />}
+        </Base>
+    );
+}
+
+function FirstStep(props){
+
+    function next(){
+        props.setStep(1)
+    }
+
+    return (
+        <>
+            <span>{props.text}</span>
+            <FiPlay onClick={next} />
+        </>
+    )
+}
+function SecondStep(props){
+
+    function next(){
+        props.setStep(2);
+    }
+
+    return (
+        <>
+            <p>{props.text}</p>
+            <IoReloadOutline onClick={next} />
+        </>
+    )
+}
+function ThirdStep(props){
+
+    const {respondidas, setRespondidas} = props.ZapStatus;
+
+    function finish(value){
+        props.setStep(3);
+        props.setChoice(value);
+        setRespondidas(respondidas+1);
+    }
+
+    return (
+        <>
+            <p>{props.text}</p>
+            <DivZap>
+                <ButtonZap onClick={() => finish(1)} color="#FF3030">Não lembrei</ButtonZap>
+                <ButtonZap onClick={() => finish(2)} color="#FF922E">Quase não lembrei</ButtonZap>
+                <ButtonZap onClick={() => finish(3)} color="#2FBE34">Zap!</ButtonZap>
+            </DivZap>
+        </>
+    )
+}
+function FourthStep(props){
+    return (
+        <ThirdBase status={props.status}>
+            <p>{props.text}</p>
+            {props.status===1 && <BsXCircleFill />}
+            {props.status===2 && <BsQuestionCircleFill />}
+            {props.status===3 && <BsFillCheckCircleFill />}
+        </ThirdBase>
+    )
+}
+
+const Base = styled.div`
+    box-sizing: none;
+    display: flex;
+    padding: 1rem;
+    width: 80%;
+    border-radius: 8px;
+    color: #333;
+    margin-bottom: 1rem;
+    ${ props => {
+        if(props.theme === 1) {
+            return (
+                `background-color: #ffffd4;
+                justify-content: space-between;
+                flex-direction: column;
+                align-items: flex-end;
+                `
+            )
+        }
+        else if (props.theme === 2){
+            return (
+                `background-color: #ffffd4;
+                flex-direction: column;
+                align-items: center;
+                justify-content: space-between;
+                `
+            )
+        }
+        else {
+            return (
+                `background-color: #f0f0f0;
+                justify-content: space-between;
+                align-items: center;
+                `
+            )
+        }
+    }};
+    p, span{
+        font-weight: 700;
+    }
+    p{
+        width: 100%;
+        text-align: start;
+        margin-bottom: 1rem;
+    }
+`;
+
+const ButtonZap = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    border-radius: 8px;
+    background-color: ${props => props.color};
+    width: calc(100% / 3.2);
+    height: 3rem;
+    font-size: 1rem;
+`;
+const DivZap = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`;
+const ThirdBase = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    color: ${props => {
+        if(props.status === 1){ return "#FF3030;" }
+        else if(props.status === 2){ return "#FF922E;" }
+        else if(props.status === 3){ return "#2FBE34;" }
+    }};
+    text-decoration: line-through;
+`;
